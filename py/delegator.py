2fa99f15e7fb98b6592ac891470daa54196f7b12
James Endicott <endicott.james@gmail.com>
2017-06-19 15:23:38 -0600

    Created utils.py based on mi Updated delegator.py to use new utils.py

--- py/delegator.py
+++ py/delegator.py
@@ -1,51 +1,39 @@
 import os
-from shutil import rmtree
 from datetime import datetime
 from functools import partial
-
-import subprocess
-
-from py.configurator import ConfigSettings, CONVERT_TASK, TRAIN_TASK, INFERENCE_TASK, SIMILARITY_TASK
+from shutil import rmtree
+from py.utils import *
+from py.configurator import ConfigSettings
 from py.file_converter import FileConverter
-from py.topic_trainer import TopicTrainer
 from py.inferencer import Inferencer
-from py.similarity_calculator import SimilarityCalculator
+from py.topic_trainer import TopicTrainer
+from py.sim_calculator import LDASim
 
+start_time = datetime.now()
+announcer = partial(announcer, process="Delegator", start=start_time)
 
-def echo_message(msg, process, start):
-    diff = datetime.now()-start
-    days = diff.days
-    hours = diff.seconds//3600
-    minutes = diff.seconds//60 - hours * 60
-    seconds = diff.seconds - minutes * 60
-    print("{process:<20}{ts:<20}{msg:<39}".format(process=process, ts="{:02d}d {:02d}h {:02d}m {:02d}s".format(days, hours, minutes, seconds), msg=msg))
+cfg = ConfigSettings()
+announcer("Loaded Configuration")
 
+os.makedirs(cfg.temp_dir)
+announcer("Created temp directory at {}".format(cfg.temp_dir))
 
-if __name__ == "__main__":
-    start_time = datetime.now()
-    announcer = partial(echo_message, process="Delegator", start=start_time)
-    cfg = ConfigSettings()
-    announcer("Loaded Configuration")
-
-    os.makedirs(cfg.temp_dir)
-    announcer("Created temp directory at {}".format(cfg.temp_dir))
-
+try:
     for task in cfg.tasks:
-        if task.type == CONVERT_TASK:
-            t = FileConverter(task, partial(echo_message, start=start_time))
-        elif task.type == INFERENCE_TASK:
-            t = Inferencer(task, partial(echo_message, start=start_time))
-        elif task.type == TRAIN_TASK:
-            t = TopicTrainer(task, partial(echo_message, start=start_time))
-        elif task.type == SIMILARITY_TASK:
-            t = SimilarityCalculator(task, partial(echo_message, start=start_time))
+        if task.type == TASK_TYPE.CONVERT:
+            t = FileConverter(task, partial(announcer, start=start_time))
+        elif task.type == TASK_TYPE.INFER:
+            t = Inferencer(task, partial(announcer, start=start_time))
+        elif task.type == TASK_TYPE.TRAIN:
+            t = TopicTrainer(task, partial(announcer, start=start_time))
+        elif task.type == TASK_TYPE.SIMILARITY:
+            t = LDASim(task, partial(announcer, start=start_time))
         else:
             raise Exception("Task was an invalid type")
         t.main()
         announcer("Finished Task")
     announcer("Finished all tasks")
-
+finally:
     rmtree(cfg.temp_dir)
     announcer("Removed temp directory")
-
-    announcer("Done")
+announcer("Done")
---
+++ py/utils.py
@@ -0,0 +1,42 @@
+import subprocess
+from functools import wraps
+from datetime import datetime
+from enum import Enum
+
+TASK_TYPE = Enum('TASK_TYPE', 'CONVERT INFER TRAIN SIMILARITY')
+PAIR_MODE = Enum('PAIR_MODE', 'ALL CROSS LIST')
+OUTPUT_FORMAT = Enum('OUTPUT_FORMAT', 'H5 CSV')
+
+
+def run_cmd(cmd, raw=False):
+    if raw:
+        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    else:
+        subprocess.run(["bash", "-c", cmd], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+def announcer(msg, process, start):
+    diff = datetime.now()-start
+    days = diff.days
+    hours = diff.seconds//3600
+    minutes = diff.seconds//60 - hours * 60
+    seconds = diff.seconds - minutes * 60 - hours * 3600
+    print("{process:<20}{ts:<20}{msg:<39}".format(process=process,
+                                                  ts="{:02d}d {:02d}h {:02d}m {:02d}s".format(days,
+                                                                                              hours,
+                                                                                              minutes,
+                                                                                              seconds),
+                                                  msg=msg))
+
+
+def memoize(obj):
+    cache = obj.cache = {}
+
+    @wraps(obj)
+    def memoizer(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = ret = obj(*args)
+            return ret
+    return memoizer
